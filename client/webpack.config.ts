4ac56f0d487ba2b50cea14a7c14e44d524806efa
ruslan <r1tu@yandex.ru>
2024-01-20 12:41:37 +0300

    init webpack , react

---
+++ client/webpack.config.ts
@@ -0,0 +1,34 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        port: 3000,
+        open: true,
+        historyApiFallback: true,
+        hot: true
+    },
+};
