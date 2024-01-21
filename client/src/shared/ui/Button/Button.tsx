39d9eed7bb863dfeddb7f1d5316f7e700e92025c
ruslan <r1tu@yandex.ru>
2024-01-21 11:24:12 +0300

    add custom button

---
+++ client/src/shared/ui/Button/Button.tsx
@@ -0,0 +1,20 @@
+import React, {ButtonHTMLAttributes} from 'react'
+import styles from './Button.module.scss'
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = (props: ButtonProps) => {
+  const {children,
+    ...otherProps} = props
+
+  return (
+    <button
+      {...otherProps}
+      className={styles.button}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
